f706a5f2801afff0b8a34077510736653c778a87
Ruan Vinicius <contatoruanvinicius@gmail.com>
2024-01-12 18:47:13 -0300

    - Finish employees controller

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\Employees\StoreEmployeeRequest;
 use App\Http\Requests\Employees\UpdateEmployeeRequest;
 use App\Models\Employee;
 use App\Services\Employees\EmployeeCreationService;
+use Illuminate\Http\JsonResponse;
 
 class EmployeeController extends Controller
 {
@@ -13,12 +14,14 @@ class EmployeeController extends Controller
     public function __construct(private readonly EmployeeCreationService $employeeCreationService)
     { }
 
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        return response()->json([
+            'data' => auth()->user()->ong->employees
+        ]);
     }
 
-    public function store(StoreEmployeeRequest $request)
+    public function store(StoreEmployeeRequest $request): JsonResponse
     {
         $this->employeeCreationService->setOng($request->user()->ong);
         $this->employeeCreationService->setData($request->all());
@@ -28,19 +31,27 @@ class EmployeeController extends Controller
         ]);
     }
 
-    public function show(Employee $employee)
+    public function show(Employee $employee): JsonResponse
     {
-        //
+        return response()->json([
+            'data' => $employee
+        ]);
     }
 
 
-    public function update(UpdateEmployeeRequest $request, Employee $employee)
+    public function update(UpdateEmployeeRequest $request, Employee $employee): JsonResponse
     {
-        //
+        $employee->update($request->all());
+        return response()->json([
+            'data' => $employee
+        ]);
     }
 
     public function destroy(Employee $employee)
     {
-        //
+        $employee->delete();
+        return response()->json([
+            'data' => $employee
+        ]);
     }
 }
