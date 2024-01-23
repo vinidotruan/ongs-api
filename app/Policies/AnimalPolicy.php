92393c6eaa02f5d7300d01556f3162a05ad06c20
Ruan Vinicius <contatoruanvinicius@gmail.com>
2024-01-23 19:47:50 -0300

    - Optimeze the animal policy

--- app/Policies/AnimalPolicy.php
+++ app/Policies/AnimalPolicy.php
@@ -10,63 +10,45 @@ class AnimalPolicy
 {
     public function viewAny(User $user): bool
     {
-        if(!$user->ong()) {
-            return false;
-        }
-
         return true;
     }
 
     public function view(User $user, Animal $animal): bool
     {
-        if($user->customer() && !$user->customer->animals->contains($animal->id)) {
-            return false;
-        }
-
         return true;
     }
 
     public function create(User $user): bool
     {
-        if(!$user->customer()) {
-            return false;
-        }
         return true;
     }
 
     public function update(User $user, Animal $animal): bool
     {
-        if($user->customer() && !$user->customer->animals->contains($animal->id)) {
-            return false;
-        }
-
-        return true;
+        return $this->isCustomerAndAnimalBelongsToCustomer($user, $animal) || $user->ong()->exists();
     }
 
     public function delete(User $user, Animal $animal): bool
     {
-        if($user->customer() && !$user->customer->animals->contains($animal->id)) {
-            return false;
-        }
-
-        return true;
+        return $this->isCustomerAndAnimalBelongsToCustomer($user, $animal) || $user->ong()->exists();
     }
 
     public function restore(User $user, Animal $animal): bool
     {
+        return $this->isCustomerAndAnimalBelongsToCustomer($user, $animal) || $user->ong()->exists();
+    }
+
+    public function forceDelete(User $user, Animal $animal): bool
+    {
+       return $this->isCustomerAndAnimalBelongsToCustomer($user, $animal) || $user->ong()->exists();
+    }
+
+    private function isCustomerAndAnimalBelongsToCustomer(User $user, Animal $animal): bool
+    {
         if($user->customer() && !$user->customer->animals->contains($animal->id)) {
             return false;
         }
 
         return true;
-    }
-
-    public function forceDelete(User $user, Animal $animal): bool
-    {
-       if($user->customer() && !$user->customer->animals->contains($animal->id)) {
-            return false;
-       }
-
-       return true;
     }
 }
